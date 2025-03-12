18c40c9d1fabf1d0fb810c57b27be9512fa1f957
Will Pittenger <WillPittenger@users.noreply.github.com>
2025-03-12 03:56:00 -0500

    Inital version
    Trying to understand how to use PythonNet to run yt-dlp's source code.  As is, VS isn't copying pythonnet's runtime dll, python.runtime.dll to my output folder from the nuget dependency location.

---
+++ Post-Build.ps1
@@ -0,0 +1,34 @@
+﻿param
+(
+	[Parameter(Position = 1)] $ProjectName,
+	[Parameter(Position = 2)] $TargetDir,
+	[Parameter(Position = 3)] $ProjectDir
+)
+
+trap
+{
+	Write-Error $_ -ErrorAction SilentlyContinue;
+
+	exit 1;
+}
+
+$ErrorActionPreference = "Stop";
+
+$dest = [IO.Path]::Combine($TargetDir, $ProjectName);
+
+$dirPSD1 = [IO.Path]::Combine($ProjectDir, $ProjectName);
+
+Write-host "Copying DLLs from $TargetDir and the PSD1 from $dirPSD1 to $dest.";
+
+if([IO.Directory]::Exists($dest))
+{
+	del ([IO.Path]::Combine($dest, '*')) -Force -Verbose;
+}
+else
+{
+	New-Item -ItemType Directory $dest -Force -Verbose;
+}
+
+dir ([IO.Path]::Combine($TargetDir, '*.dll')), ([IO.Path]::Combine($dirPSD1, '*')) | %{Copy $_ ([io.path]::combine($dest, $_.Name)) -Force -Verbose};
+
+exit 0;
---
+++ TestYtDlp.cs
@@ -0,0 +1,33 @@
+﻿// Ignore Spelling: Dlp
+
+using Python.Runtime;
+
+namespace YtDlpTest;
+
+[System.Management.Automation.Cmdlet(System.Management.Automation.VerbsDiagnostic.Test, "YtDlp")]
+public class TestYtDlp : System.Management.Automation.Cmdlet
+{
+	public TestYtDlp()
+	{
+		Runtime.PythonDLL = "python37.dll";
+
+		PythonEngine.Initialize();
+		PythonEngine.BeginAllowThreads();
+
+		yt = Py.Import("yt_dlp");
+	}
+
+	~TestYtDlp()
+		=> PythonEngine.Shutdown();
+
+	private readonly dynamic yt;
+
+	protected override void ProcessRecord()
+	{
+		base.ProcessRecord();
+
+		using Py.GILState lockInfo = Py.GIL();
+
+		WriteObject(yt.version.__version__);
+	}
+}
